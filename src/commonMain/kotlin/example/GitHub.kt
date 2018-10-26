31df4fc7e697649ea0ea19834afb643d0049de89
bootstraponline <matt@bootstraponline.com>
2018-10-26 11:35:14 -0400

    Replace try/finally with .use

--- src/commonMain/kotlin/example/GitHub.kt
+++ src/commonMain/kotlin/example/GitHub.kt
@@ -4,6 +4,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.HttpClientCall
 import io.ktor.client.call.call
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.io.core.use
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
@@ -12,13 +13,5 @@ import kotlin.coroutines.EmptyCoroutineContext
 expect fun <T> runBlocking(context: CoroutineContext = EmptyCoroutineContext, block: suspend CoroutineScope.() -> T): T
 
 fun getGitHub(): HttpClientCall = runBlocking {
-    val client = HttpClient()
-    val result: HttpClientCall
-    try {
-        result = client.call("https://www.github.com")
-    } finally {
-        client.close()
-    }
-
-    result
+    HttpClient().use { it.call("https://www.github.com") }
 }
