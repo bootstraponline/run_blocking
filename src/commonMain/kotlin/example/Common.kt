3478db5c884cb8d6646e1cd49ce678e96fa15591
bootstraponline <code@bootstraponline.com>
2019-12-08 12:25:03 -0800

    Add native runBlocking on iOS

--- src/commonMain/kotlin/example/Common.kt
+++ src/commonMain/kotlin/example/Common.kt
@@ -1,16 +1,18 @@
 package example
 
 import io.ktor.client.HttpClient
-import io.ktor.client.call.call
-import io.ktor.client.response.readText
-import kotlinx.io.core.use
+import io.ktor.client.request.request
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.readText
+import io.ktor.utils.io.core.use
 
 expect fun <T> runBlocking(block: suspend () -> T): T
 
 fun healthCheck(): String {
     return runBlocking {
         HttpClient().use {
-            it.call("http://robinhood.com/healthcheck").response.readText()
+            it.request<HttpResponse>("http://robinhood.com/healthcheck") {}
+                .readText()
         }
     }
 }
--- src/iosMain/kotlin/example/RunBlocking.kt
+++ src/iosMain/kotlin/example/RunBlocking.kt
@@ -1,27 +1,6 @@
 package example
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Runnable
-import kotlinx.coroutines.launch
-import platform.Foundation.NSRunLoop
-import platform.Foundation.performBlock
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.runBlocking
 
-actual fun <T> runBlocking(block: suspend () -> T): T {
-    val expectation = Expectation<T>()
+actual fun <T> runBlocking(block: suspend () -> T): T = runBlocking { block.invoke() }
 
-    GlobalScope.launch(MainRunLoopDispatcher) {
-        expectation.fulfill(block.invoke())
-    }
-
-    return expectation.wait() ?: throw RuntimeException("runBlocking failed")
-}
-
-private object MainRunLoopDispatcher : CoroutineDispatcher() {
-    override fun dispatch(context: CoroutineContext, block: Runnable) {
-        NSRunLoop.mainRunLoop().performBlock {
-            block.run()
-        }
-    }
-}
