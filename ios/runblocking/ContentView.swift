37404577d467507e52abb7fd5cd23c1a041e2c5f
Janek Góral <jan.goral@gogoapps.io>
2020-01-31 14:58:54 +0100

    Add fresh ios project

---
+++ ios/runblocking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  runblocking
+//
+//  Created by Janek Góral on 31/01/2020.
+//  Copyright © 2020 gogoapps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
